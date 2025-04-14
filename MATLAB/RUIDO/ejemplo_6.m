e5886349e268cc9c67933e4d41c2d868c0d0f2db
dbsrunsaac <dbsrunsaac@unsaac.edu.pe>
2025-04-14 08:31:45 -0500

    Arreglando el ruido

--- MATLAB/RUIDO/ejemplo_6.m
+++ MATLAB/RUIDO/ejemplo_6.m
@@ -1,9 +1,10 @@
 clc, clear, close all;
+
 % Añadir ruido a una sinusoide usando números aleatorios (randn - distrib. normal)
-randn('state',0);
-t = 0:0.001:pi;
-g = sin(2*pi*2*t);
-g_ruido = g + 0.25*randn(1,size(t));   % sumar vector aleatorio
+randn('state', 0); % Establecer la semilla para la generación de números aleatorios
+t = 0:0.001:pi; % Vector de tiempo de 0 a pi con incremento de 0.001
+g = sin(2*pi*2*t); % Sinusoide de 2 Hz
+g_ruido = g + 0.25*randn(size(t)); % Sumar vector aleatorio con distribución norma
 
 subplot(211)
 plot(t,g)
