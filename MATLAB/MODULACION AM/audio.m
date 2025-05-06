1067277b8f0757b6b8e3d62f2aae8b9dd5a7978c
dbsrunsaac <dbsrunsaac@unsaac.edu.pe>
2025-05-05 22:17:52 -0500

    audio recorder

--- MATLAB/MODULACION AM/audio.m
+++ MATLAB/MODULACION AM/audio.m
@@ -1,4 +1,19 @@
+clear, clc;
+
 recObj = audiorecorder
 recordblocking(recObj, 5);
 
-play(recObj)
+play(recObj);
+
+
+y = getaudiodata(recObj);
+
+plot(y);
+
+Fs = 8000;
+audiowrite('grabacion.wav', y, Fs);
+
+
+% Recuperar el archivo en matlab
+
+[y, Fs] = audioread('grabacion.wav');
