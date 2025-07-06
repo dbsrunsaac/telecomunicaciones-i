87e26c1b5cd28eca23c7cfb2d9099f33fbe9fefa
dbsalazarr <dbsalazarr@gmail.com>
2025-07-06 17:42:47 -0500

    Modulación angular - 1st

--- MATLAB/6. MODULACION FM/modulacion_pm_fm.m
+++ MATLAB/6. MODULACION FM/modulacion_pm_fm.m
@@ -3,17 +3,18 @@ clc, clear, close all;
 %% GENERACION DE LAS SEÑALES
 clc; clear; close all;
 
-f = 15;               % Frecuencia de la señal (Hz)
+f = 0.25;               % Frecuencia de la señal (Hz)
 T = 1/f;             % Periodo
 Fs = 1000;           % Frecuencia de muestreo
 Ts = 1/Fs;
 t = 0: Ts : 10*T;      % Vector de tiempo para dos periodos
 
+
 % SENAL CUADRADA
 ft1 = square(2*pi*f*t);
 
 % SENAL TRIANGULAR
-ft2 = sawtooth(2*pi*f*t, 0.5);  % Ciclo de subida y bajada simetrico
+ft2 = 1 + sawtooth(2*pi*f*t, 0.5);  % Ciclo de subida y bajada simetrico
 
 
 %% MODULACION PM Y FM
@@ -33,10 +34,16 @@ fi4 = A*sin( 2*pi*fc*t + cumsum(ft2));
 
 %% GRAFICAS Y RESULTADOS
 figure;
+subplot(1, 2, 1);
+plot(t, ft1);
+title("Señal Cuadrada");
 
+subplot(1, 2, 2);
+plot(t, ft2);
+title("Señal Triangular");
+
+figure;
 subplot(2,2,1);
-plot(t, ft1, 'b', 'LineWidth', 1.5);
-hold on
 plot(t, fi1);
 title('Señal Cuadrada');
 xlabel('Tiempo [s]');
@@ -46,8 +53,6 @@ grid on;
 hold off;
 
 subplot(2,2,2);
-plot(t, ft2, 'r', 'LineWidth', 1.5);
-hold on
 plot(t, fi2);
 title('Señal Triangular');
 xlabel('Tiempo [s]');
@@ -57,8 +62,6 @@ grid on;
 hold off;
 
 subplot(2, 2, 3);
-plot(t, ft1);
-hold on
 plot(t, fi3);
 title('Señal Cuadrada');
 xlabel('Tiempo [s]');
@@ -68,8 +71,6 @@ grid on;
 hold off;
 
 subplot(2, 2, 4);
-plot(t, ft2);
-hold on
 plot(t, fi4);
 title('Señal Triangular');
 xlabel('Tiempo [s]');
